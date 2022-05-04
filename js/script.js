aaf0b960be0271eb1756b8d73e910794d759b34f
Artem Oliynyk <oliynykartemfr@gmail.com>
2022-05-04 16:06:02 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// webp
 function testWebP(callback) {
 	var webP = new Image();
 	webP.onload = webP.onerror = function () {
@@ -8,31 +9,42 @@ function testWebP(callback) {
 testWebP(function (support) {
 	if (support == true) {
 		document.querySelector('body').classList.add('webp');
-	}else{
+	} else {
 		document.querySelector('body').classList.add('no-webp');
 	}
 });
 // header burger	
-function burger(){
-	let header__burger = document.querySelector('.header__burger'),
-		 header__menu = document.querySelector('.header__menu'),
-		 header = document.querySelector('.header');
-	header__burger.addEventListener('click', function(){
-		if (header__burger.classList.contains('active'), header__menu.classList.contains('active'), header.classList.contains('active')){
-			header__burger.classList.remove('active') || header__menu.classList.remove('active') || header.classList.remove('active')
+function burgerMenu() {
+	const burger = document.querySelector('.burger')
+	const menu = document.querySelector('.menu')
+	const body = document.querySelector('body')
+	burger.addEventListener('click', () => {
+		if (!menu.classList.contains('active')) {
+			menu.classList.add('active')
+			burger.classList.add('active')
+			body.classList.add('locked')
 		} else {
-			header__burger.classList.add('active') || header__menu.classList.add('active') || header.classList.add('active')
+			menu.classList.remove('active')
+			burger.classList.remove('active')
+			body.classList.remove('locked')
+		}
+	})
+	window.addEventListener('resize', () => {
+		if (window.innerWidth > 1110) {
+			menu.classList.remove('active')
+			burger.classList.remove('active')
+			body.classList.remove('locked')
 		}
 	})
 }
-burger();
+burgerMenu();
 // body__swiper
-if (document.querySelector('.body__swiper')){
+if (document.querySelector('.body__swiper')) {
 	// review
 	new Swiper(".swiper-container", {
 		direction: "horizontal",
 		loop: !0,
-		initialSlide: 0,
+		cssMode: true,
 		speed: 1500,
 		slidesPerView: 1,
 		spaceBetween: 100,
@@ -51,7 +63,7 @@ if (document.querySelector('.body__swiper')){
 	})
 }
 // body__room
-if (document.querySelector('.body__room')){
+if (document.querySelector('.body__room')) {
 	// room swiper
 	new Swiper(".swiper-container1", {
 		direction: "horizontal",
@@ -74,12 +86,12 @@ if (document.querySelector('.body__room')){
 		},
 	})
 	// accordion
-	function accordion(){
+	function accordion() {
 		const items = document.querySelectorAll('.accordion__header')
 		items.forEach(item => {
 			item.addEventListener("click", () => {
 				const parent = item.parentNode
-				if (parent.classList.contains('show')){
+				if (parent.classList.contains('show')) {
 					parent.classList.remove('show')
 				} else {
 					document
@@ -93,32 +105,26 @@ if (document.querySelector('.body__room')){
 	accordion()
 }
 // body__scroll
-if (document.querySelector('.body__scroll')){
+if (document.querySelector('.body__scroll')) {
 	//scroll to
 	new SmoothScroll('a[href*="#"]')
 }
 // animation on start
 const tl = new TimelineMax()
-const mySplitText = new SplitText('.start__text > h1, .start__text > h2, .start__text > h3, .start__text > p, .start__contact > h1', {type: 'chars'})
+const mySplitText = new SplitText('.start__text > h4, .start__text > h2, .start__text > h3, .start__text > p, .start__contact > h2', { type: 'chars' })
+const mySplitText1 = new SplitText('.start__text > p', { type: 'chars' })
 const chars = mySplitText.chars
-tl.from('.header', .7, {y: '-100%'})
-tl.staggerFrom(chars, .4, {opacity: 0}, .1, ">-0.6")
-tl.from('.start__button', .7, {y: '150%'}, "-=8.4")
-// animation on scroll
+const chars1 = mySplitText1.chars
+tl.from('.header', .7, { y: '-100%' })
+tl.staggerFrom(chars, .4, { opacity: 0 }, .1, ">-0.6")
+tl.staggerFrom(chars1, .4, { opacity: 0 }, .03, ">-6")
+
+let left = document.querySelector('.content-left')
+let bottom = document.querySelector('.content-bottom')
+let right = document.querySelector('.content-right')
+let opacity = document.querySelector('.content-opacity')
 gsap.registerPlugin(ScrollTrigger);
-gsap.utils.toArray('.content-left').forEach((el, index) => { 
-  let tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: el,
-      start: "top bottom",
-      toggleActions: "play none none reverse",
-    }
-  })
-  tl
-  .set(el, {transformOrigin: 'center center'})
-  .fromTo(el, {x: '-20%'}, {x: "0%", duration: .8, immediateRender: false})
-})
-gsap.utils.toArray('.content-bottom').forEach((el, index) => { 
+gsap.utils.toArray(left).forEach((el, index) => {
 	let tl = gsap.timeline({
 		scrollTrigger: {
 			trigger: el,
@@ -127,30 +133,42 @@ gsap.utils.toArray('.content-bottom').forEach((el, index) => {
 		}
 	})
 	tl
-	.set(el, {transformOrigin: 'center center'})
-	.fromTo(el, {y: '50%'}, {y: "0%", duration: .8, immediateRender: false})
- })
-gsap.utils.toArray('.content-right').forEach((el, index) => { 
-  let tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: el,
-      start: "top bottom",
-      toggleActions: "play none none reverse",
-    }
-  })
-  tl
-  .set(el, {transformOrigin: 'center center'})
-  .fromTo(el, {x: '20%'}, {x: "0%", duration: .8, immediateRender: false})
+		.set(el, { transformOrigin: 'center center' })
+		.fromTo(el, { x: '-100px' }, { x: "0", duration: .8, immediateRender: false })
 })
-gsap.utils.toArray('.content-opacity').forEach((el, index) => { 
+gsap.utils.toArray(bottom).forEach((el, index) => {
 	let tl = gsap.timeline({
-	  scrollTrigger: {
-		 trigger: el,
-		 start: "top bottom",
-		 toggleActions: "play none none reverse",
-	  }
+		scrollTrigger: {
+			trigger: el,
+			start: "top bottom",
+			toggleActions: "play none none reverse",
+		}
 	})
 	tl
-	.set(el, {transformOrigin: 'center center'})
-	.fromTo(el, {opacity: 0}, {opacity: 1, duration: 1, immediateRender: false})
- })
+		.set(el, { transformOrigin: 'center center' })
+		.fromTo(el, { y: '100px' }, { y: "0", duration: .8, immediateRender: false })
+})
+gsap.utils.toArray(right).forEach((el, index) => {
+	let tl = gsap.timeline({
+		scrollTrigger: {
+			trigger: el,
+			start: "top bottom",
+			toggleActions: "play none none reverse",
+		}
+	})
+	tl
+		.set(el, { transformOrigin: 'center center' })
+		.fromTo(el, { x: '100px' }, { x: "0", duration: .8, immediateRender: false })
+})
+gsap.utils.toArray(opacity).forEach((el, index) => {
+	let tl = gsap.timeline({
+		scrollTrigger: {
+			trigger: el,
+			start: "top bottom",
+			toggleActions: "play none none reverse",
+		}
+	})
+	tl
+		.set(el, { transformOrigin: 'center center' })
+		.fromTo(el, { opacity: 0 }, { opacity: 1, duration: 1, immediateRender: false })
+})
